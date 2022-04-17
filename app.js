03e4f4dbe213abf30a99c084e4ce61c1b9087c04
ricepotato <sukjun40@naver.com>
2022-04-17 16:35:19 +0900

    fix: bug snapshot

---
+++ app.js
@@ -0,0 +1,92 @@
+function App(app) {
+  this.state = {
+    videos: [],
+  };
+
+  this.setState = (newState) => {
+    this.state = newState;
+    this.render();
+  };
+
+  this.render = () => {
+    new ListView({ target: app, initialState: this.state });
+  };
+
+  this.render();
+}
+
+function ListView({ target, initialState }) {
+  this.state = initialState;
+  const setState = (newState) => {
+    this.state = newState;
+    this.render();
+  };
+  const render = () => {
+    target.innerHTML = "";
+    const ul = document.createElement("ul");
+    ul.setAttribute("class", "list-view");
+
+    this.state.videos.map((item) => {
+      const li = document.createElement("li");
+      li.setAttribute("class", "list-item");
+      new ListItem({ target: li, initialState: item });
+      ul.appendChild(li);
+    });
+
+    target.appendChild(ul);
+  };
+  render();
+}
+
+function ListItem({ target, initialState }) {
+  //this.state = { target: undefined, snapshots: [] };
+  this.state = { ...initialState };
+  this.currentIndex = 0;
+  const setState = (newState) => {
+    this.state = newState;
+    this.render();
+  };
+  const render = () => {
+    target.innerHTML = `
+    <div class="list-item__container">
+      <div class="list-item__image_container">
+        <img src='${this.state.snapshots[this.currentIndex]}'/>
+      </div>
+      <div class="list-item__label_container">
+        <span>
+          <a target="_blank" href="potplayer://${this.state.target.replace(
+            /\\/gi,
+            "/"
+          )}">${this.state.name}</a>
+        </span>
+      </div>
+      <div>
+      ${this.state.target}
+      </div>
+    </div>
+    `;
+
+    const img = target.querySelector("img");
+    img.addEventListener("click", () => {
+      console.log(this.state.target);
+      this.currentIndex++;
+      img.src =
+        this.state.snapshots[this.currentIndex % this.state.snapshots.length];
+    });
+  };
+  render();
+}
+
+const btn = document.getElementById("btnOpen");
+const appElement = document.getElementById("app");
+const app = new App(appElement);
+
+app.setState({ videos: videos });
+
+btn.addEventListener("click", async () => {
+  const [fileHandle] = await window.showOpenFilePicker();
+  const file = await fileHandle.getFile();
+  const contents = await file.text();
+  const dataJson = JSON.parse(contents);
+  app.setState({ videos: dataJson });
+});
--- main.py
+++ main.py
@@ -24,17 +24,22 @@ class Snapshot:
         return f"Snapshot({self.mp4_filepath}) - {ssc} snapshots"
 
     def to_dict(self):
-        return {"target": self.mp4_filepath, "snapshots": self.snapshots}
+        return {
+            "name": os.path.split(self.mp4_filepath)[-1],
+            "target": self.mp4_filepath,
+            "snapshots": self.snapshots,
+        }
 
 
-def get_filelist(root_path: str, ext: str = None) -> Generator[Snapshot, None, None]:
+def get_filelist(root_path: str, ext: str = None) -> Generator[str, None, None]:
     for (path, _, files) in os.walk(root_path):
         for filename in files:
             if ext is not None:
                 _, file_ext = os.path.splitext(filename)
                 if file_ext.lower() != ext:
                     continue
-            yield Snapshot(os.path.join(path, filename))
+            # yield Snapshot(os.path.join(path, filename))
+            yield os.path.join(path, filename)
 
 
 def get_snapshots_filelist(root_path: str) -> Generator[str, None, None]:
@@ -53,17 +58,19 @@ def make_snapshot_dir(filepath, target_path) -> str:
     if not os.path.exists(new_dir):
         os.makedirs(new_dir)
         log.info("snapshot dir created. %s", new_dir)
+    else:
+        log.warning("snapshot dir already exists. %s", new_dir)
     return new_dir
 
 
-def process_snapshot(ss_obj: Snapshot):
-    log.info("processing... %s", ss_obj)
-    snapshot_dir = make_snapshot_dir(ss_obj.mp4_filepath, target_path)
+def process_snapshot(mp4_filepath: str) -> Snapshot:
+    log.info("processing... %s", mp4_filepath)
+    snapshot_dir = make_snapshot_dir(mp4_filepath, target_path)
     snapshots = list(get_filelist(snapshot_dir, ".jpg"))
     if len(snapshots) <= 0:
         log.warning("snapshot not exist. %s", snapshot_dir)
-        snapshots = make_snapshot(ss_obj.mp4_filepath, out_path=snapshot_dir, width=250)
-    res = Snapshot(ss_obj.mp4_filepath)
+        snapshots = make_snapshot(mp4_filepath, out_path=snapshot_dir, width=250)
+    res = Snapshot(mp4_filepath)
     res.snapshots = snapshots
     return res
 
@@ -74,6 +81,9 @@ def dump_jsonfile(snapshots: List[Snapshot], output_path):
     json_filepath = os.path.join(output_path, "snapshots.json")
     with open(json_filepath, "w") as f:
         f.write(json_str)
+    js_filepath = os.path.join(output_path, "snapshots.js")
+    with open(js_filepath, "w") as f:
+        f.write(f"const videos={json_str}")
 
 
 def main():
