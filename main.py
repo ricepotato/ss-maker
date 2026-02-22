377419db7f1b85718c8514836ce2fb6af240167c
ricepotato <sukjun40@naver.com>
2026-02-22 19:18:18 +0900

    feat: dirs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import hashlib
 import os
 import json
@@ -6,8 +7,11 @@ import multiprocessing
 import shutil
 import argparse
 import sys
+import pathlib
+import pydantic
 from typing import Generator, List
 from snapshot import make_snapshot
+from concurrent.futures import ProcessPoolExecutor
 
 
 log = logging.getLogger("app")
@@ -15,7 +19,13 @@ log.addHandler(logging.StreamHandler())
 log.setLevel(logging.DEBUG)
 log = logging.getLogger(f"app.{__name__}")
 
-CHUNK_SIZE = 1024 * 1024
+CHUNK_SIZE = 1024 * 1024 * 10
+
+
+class Target(pydantic.BaseModel):
+    files: List[pathlib.Path]
+    dirs: List[pathlib.Path]
+  
 
 
 def get_file_hash(filepath):
@@ -27,35 +37,71 @@ def get_file_hash(filepath):
 
 
 class Snapshot:
-    def __init__(self, mp4_filepath: str):
-        self.mp4_filepath = mp4_filepath
+    def __init__(self, target: pathlib.Path):
+        self.target = target
         self.snapshots: List[str] = []
-        self.sha256 = get_file_hash(mp4_filepath)
-        self.dirname = os.path.split(os.path.dirname(mp4_filepath))[-1]
+        self._sha256 = None
+        self._snapshot_path = self.target.parent / ".snapshots"
+
+    @property
+    def sha256(self):
+        if self._sha256 is None:
+            self._sha256 = get_file_hash(self.target)
+        return self._sha256
+    
+    def make_snapshot_path(self):
+        if not os.path.exists(self._snapshot_path):
+            os.makedirs(self._snapshot_path)
+
+    def make(self):
+        self.make_snapshot_path()
+        out_path = self._snapshot_path / self.sha256
+        shot_count = 16
+        if not os.path.exists(out_path):
+            os.makedirs(out_path)
+            snapshot_paths = make_snapshot(str(self.target), out_path=str(out_path), shot_count=shot_count, width=250)
+            self.snapshots = snapshot_paths 
+        else:
+            log.warning("snapshot dir already exists. skip... %s", out_path)
+            self.snapshots = get_file_list(out_path, ".jpg")
+        return self
+
+    @property
+    def name(self) -> str:
+        return self.target.stem
 
     def __str__(self):
         ssc = len(self.snapshots)
-        return f"Snapshot({self.mp4_filepath}) - {ssc} snapshots"
-
+        return f"Snapshot({self.target}) - {ssc} snapshots"
+    
+    def __repr__(self):
+        return self.__str__()            
+    
     def to_dict(self):
         return {
-            "name": os.path.split(self.mp4_filepath)[-1],
-            "target": self.mp4_filepath,
+            "name": self.target.stem,
+            "target": self.target.as_posix(),
             "snapshots": self.snapshots,
             "sha256": self.sha256,
-            "dirname": self.dirname,
+            "dirname": self.target.parent.name
         }
 
+def get_file_list(root_path: str, ext: str = None):
+    return [os.path.join(root_path, item) for item in os.listdir(root_path)]
 
-def get_filelist(root_path: str, ext: str = None) -> Generator[str, None, None]:
-    for (path, _, files) in os.walk(root_path):
-        for filename in files:
-            if ext is not None:
-                _, file_ext = os.path.splitext(filename)
-                if file_ext.lower() != ext:
-                    continue
-            # yield Snapshot(os.path.join(path, filename))
-            yield os.path.join(path, filename)
+def get_target_list(root_path: str, ext: str = None) -> Target:
+    files = []
+    dirs = []
+    for item in os.listdir(root_path):
+        item_path = os.path.join(root_path, item)
+        if os.path.isdir(item_path):
+            dirs.append(pathlib.Path(item_path))
+        else:
+            _, file_ext = os.path.splitext(item)
+            if ext is not None and file_ext.lower() != ext:
+                continue
+            files.append(pathlib.Path(item_path))
+    return Target(files=files, dirs=dirs)
 
 
 def get_snapshots_filelist(root_path: str) -> Generator[str, None, None]:
@@ -79,20 +125,19 @@ def make_snapshot_dir(filepath, target_path, sha256) -> str:
     return new_dir
 
 
-def process_snapshot(arg) -> Snapshot:
-    target_path = arg[0]
-    mp4_filepath = arg[1]
+def process_snapshot(target: pathlib.Path) -> Snapshot:
+    target_path = target.parent
+    mp4_filepath = str(target)
     log.info("processing... %s", mp4_filepath)
 
-    res = Snapshot(mp4_filepath)
+    res = Snapshot(target)
     snapshot_dir = make_snapshot_dir(mp4_filepath, target_path, res.sha256)
 
-    snapshots = list(get_filelist(snapshot_dir, ".jpg"))
+    snapshots = list(get_target_list(snapshot_dir, ".jpg"))
     if len(snapshots) <= 0:
         log.warning("snapshot not exist. %s", snapshot_dir)
         snapshots = make_snapshot(mp4_filepath, out_path=snapshot_dir, width=250)
     res.snapshots = snapshots
-    # rename_snapshot(res)
     return res
 
 
@@ -102,7 +147,7 @@ def copy_to_path(filename, dest_path):
 
 
 def dump_jsonfile(snapshots: List[Snapshot], output_path):
-    ss_list = [snapshot.to_dict() for snapshot in snapshots]
+    ss_list = [snapshot.to_dict() for snapshot in snapshots if snapshot is not None]
     json_str = json.dumps(ss_list, indent=4)
     json_filepath = os.path.join(output_path, "snapshots.json")
     with open(json_filepath, "w") as f:
@@ -116,20 +161,33 @@ def dump_jsonfile(snapshots: List[Snapshot], output_path):
 
 
 def main():
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--target", required=True)
+    parser.add_argument("--recursive", default=False, action="store_true")
     args = parser.parse_args()
 
     if not os.path.exists(args.target):
         print("path not exists.", args.target)
         sys.exit(1)
 
-    mp4_filelist = get_filelist(args.target, ".mp4")
-    args_list = [[args.target, mp4_file] for mp4_file in mp4_filelist]
-    p = multiprocessing.Pool(4)
-    snapshots = p.map(process_snapshot, args_list)
-    dump_jsonfile(snapshots, args.target)
+    target = get_target_list(args.target, ".mp4")
+    snapshot_list = [Snapshot(target) for target in target.files]
+
+    results = []
+    with ProcessPoolExecutor(max_workers=4) as executor:
+        futures = []
+        for snapshot in snapshot_list:
+            future = executor.submit(snapshot.make)
+            futures.append(future)
+
+        futures_result = [future.result() for future in futures]
+
+        for result in futures_result:
+            results.append(result)
+
+    dump_jsonfile(results, args.target)
+
+
 
 
 if __name__ == "__main__":
